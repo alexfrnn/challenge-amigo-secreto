7efcc4311cb6bd65889327a1111bde28df8b4c76
alex fernando <alexfernand.o@outlook.com>
2025-03-16 11:47:25 -0300

    add function for name insertion and function to clear name input

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos =[];
 
+function adicionarAmigo() {
+  let nome = document.getElementById("amigo").value;
+  if(nome == ""){
+    alert("Por favor, insira um nome válido!");
+  }
+  amigos.push(nome);
+  console.log(amigos);
+  limparCampo();
+}
 
+function limparCampo() {
+  nome = document.getElementById("amigo");
+  nome.value = "";
+}
