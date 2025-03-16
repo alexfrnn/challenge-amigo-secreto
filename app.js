e6284848baa34792266d4d2b3a593451b109685b
alex fernando <alexfernand.o@outlook.com>
2025-03-16 14:50:44 -0300

    add a function to generate random options

--- app.js
+++ app.js
@@ -20,6 +20,18 @@ function listarNome() {
     li.innerHTML = amigos[i];
     listaNomes.appendChild(li);
   }
+
+}
+
+function sortearAmigo() {
+  if (amigos.length == 0) {
+    alert("Digite o nome de um amigo");
+  } else{
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    document.getElementById("resultado").innerHTML = "Amigo sorteado: " + amigoSorteado;
+    document.getElementById("listaAmigos").innerHTML = "";
+  }
 }
 
 function limparCampo() {
