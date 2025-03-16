379b30bbcd67795a3af5fe63f7eb024c502e12a9
alex fernando <alexfernand.o@outlook.com>
2025-03-16 13:03:24 -0300

    feat: function for list friend names in HTML; and update add function

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos =[];
+let amigos = [];
 
 function adicionarAmigo() {
   let nome = document.getElementById("amigo").value;
-  if(nome == ""){
+  if (nome == "") {
     alert("Por favor, insira um nome válido!");
   }
   amigos.push(nome);
   console.log(amigos);
   limparCampo();
+  listarNome();
+}
+
+function listarNome() {
+  let listaNomes = document.getElementById("listaAmigos");
+  listaNomes.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = amigos[i];
+    listaNomes.appendChild(li);
+  }
 }
 
 function limparCampo() {
